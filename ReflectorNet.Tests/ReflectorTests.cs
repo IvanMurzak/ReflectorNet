df51d0c65469ab64167b825a48850d247e79b8e1
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-05-27 20:25:35 -0700

    Add ReflectorNet.Tests project and initial test cases for method reflection

---
+++ ReflectorNet.Tests/ReflectorTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using com.IvanMurzak.ReflectorNet.Data.Unity;
+using com.IvanMurzak.ReflectorNet.Reflection;
+using Xunit;
+
+namespace ReflectorNet.Tests
+{
+    public class ReflectorTests
+    {
+        [Fact]
+        public void FindMethod_WithKnownMethod_ShouldReturnMethod()
+        {
+            // Arrange
+            var reflector = new Reflector();
+            var methodPointer = new MethodPointerRef
+            {
+                Namespace = typeof(TestClass).Namespace,
+                TypeName = nameof(TestClass),
+                MethodName = nameof(TestClass.NoParameters_ReturnBool)
+            };
+
+            // Act
+            var foundMethods = reflector.FindMethod(
+                methodPointer,
+                knownNamespace: true,
+                typeNameMatchLevel: 6,
+                methodNameMatchLevel: 6
+            ).ToList();
+
+            // Assert
+            Assert.Single(foundMethods);
+            Assert.Equal(nameof(TestClass.NoParameters_ReturnBool), foundMethods[0].Name);
+            Assert.Equal(typeof(bool), foundMethods[0].ReturnType);
+        }
+
+        [Fact]
+        public void FindMethod_WithPartialMethodName_ShouldReturnMatchingMethods()
+        {
+            // Arrange
+            var reflector = new Reflector();
+            var methodPointer = new MethodPointerRef
+            {
+                Namespace = typeof(TestClass).Namespace,
+                TypeName = nameof(TestClass),
+                MethodName = "Return" // Partial method name
+            };
+
+            // Act
+            var foundMethods = reflector.FindMethod(
+                methodPointer,
+                knownNamespace: true,
+                typeNameMatchLevel: 6,
+                methodNameMatchLevel: 2 // Lower level to match partial names
+            ).ToList();
+
+            // Assert
+            Assert.Contains(foundMethods, m => m.Name == nameof(TestClass.NoParameters_ReturnBool));
+        }
+    }
+}
---
+++ ReflectorNet.Tests/TestClass.cs
@@ -0,0 +1,15 @@
+namespace ReflectorNet.Tests
+{
+    public class TestClass
+    {
+        public void NoParameters_ReturnVoid()
+        {
+            // This method does nothing
+        }
+
+        public bool NoParameters_ReturnBool()
+        {
+            return true;
+        }
+    }
+}
