4272e13fd6b325f8cad81a4b512158240ab15810
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-07-26 22:26:27 -0700

    feat: update target frameworks and enhance logging functionality with new Print and Printer classes

--- ReflectorNet/src/Utils/Log/StringBuilderLogger.cs
+++ ReflectorNet/src/Utils/Log/StringBuilderLogger.cs
@@ -6,16 +6,19 @@ namespace com.IvanMurzak.ReflectorNet.Utils
 {
     public class StringBuilderLogger : ILogger
     {
-        readonly string _categoryName;
+        readonly string? _categoryName;
         readonly StringBuilder _stringBuilder;
 
-        public StringBuilderLogger(string categoryName) : this(categoryName, new StringBuilder()) { }
-        public StringBuilderLogger(string categoryName, StringBuilder stringBuilder)
+        public StringBuilderLogger(string? categoryName = null)
+            : this(new StringBuilder(), categoryName)
+        { }
+
+        public StringBuilderLogger(StringBuilder stringBuilder, string? categoryName = null)
         {
-            _categoryName = categoryName.Contains(".")
-                ? categoryName.Substring(categoryName.LastIndexOf('.') + 1)
-                : categoryName;
             _stringBuilder = stringBuilder;
+            _categoryName = categoryName?.Contains(".") == true
+                ? categoryName?.Substring(categoryName.LastIndexOf('.') + 1)
+                : categoryName;
         }
 
         IDisposable ILogger.BeginScope<TState>(TState state) => null!;
@@ -27,7 +30,21 @@ namespace com.IvanMurzak.ReflectorNet.Utils
             if (formatter == null) throw new ArgumentNullException(nameof(formatter));
             if (state == null) throw new ArgumentNullException(nameof(state));
 
-            var message = $"{Consts.Log.Tag} {Consts.Log.Color.CategoryStart}{_categoryName}{Consts.Log.Color.CategoryEnd} {Consts.Log.Color.LevelStart}[{logLevel}]{Consts.Log.Color.LevelEnd} {formatter(state, exception)}";
+            var logLevelLabel = logLevel switch
+            {
+                LogLevel.Trace => Consts.Log.Trce,
+                LogLevel.Debug => Consts.Log.Dbug,
+                LogLevel.Information => Consts.Log.Info,
+                LogLevel.Warning => Consts.Log.Warn,
+                LogLevel.Error => Consts.Log.Fail,
+                LogLevel.Critical => Consts.Log.Crit,
+                _ => throw new ArgumentOutOfRangeException(nameof(logLevel), logLevel, null)
+            };
+
+            var message = string.IsNullOrEmpty(_categoryName)
+                ? $"{Consts.Log.Tag} {Consts.Log.Color.LevelStart}[{logLevelLabel}]{Consts.Log.Color.LevelEnd} {formatter(state, exception)}"
+                : $"{Consts.Log.Tag} {Consts.Log.Color.CategoryStart}{_categoryName}{Consts.Log.Color.CategoryEnd} {Consts.Log.Color.LevelStart}[{logLevelLabel}]{Consts.Log.Color.LevelEnd} {formatter(state, exception)}";
+
             _stringBuilder.AppendLine(message);
         }
         public void Clear() => _stringBuilder.Clear();
---
+++ ReflectorNet/src/Utils/Print/Printer.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace com.IvanMurzak.ReflectorNet.Utils
+{
+    public class Printer : IDisposable
+    {
+        readonly LinkedList<LazyLine> _lazyLines = new();
+        readonly ILogger? _aiLogger;
+        readonly ILogger? _systemLogger;
+
+        int lastDepth = -1;
+
+        public Printer(ILogger? logger)
+            : this(new StringBuilderLogger(), logger)
+        { }
+
+        public Printer(ILogger? aiLogger = null, ILogger? systemLogger = null)
+        {
+            _aiLogger = aiLogger;
+            _systemLogger = systemLogger;
+        }
+
+        public void TraceLog(Func<string> lazyLine, int depth = 0)
+            => LogLine(LogLevel.Trace, lazyLine, depth);
+
+        public void DebugLog(Func<string> lazyLine, int depth = 0)
+            => LogLine(LogLevel.Debug, lazyLine, depth);
+
+        public void InfoLog(Func<string> lazyLine, int depth = 0)
+            => LogLine(LogLevel.Information, lazyLine, depth);
+
+        public void WarningLog(Func<string> lazyLine, int depth = 0)
+            => LogLine(LogLevel.Warning, lazyLine, depth);
+
+        public void ErrorLog(Func<string> lazyLine, int depth = 0)
+            => LogLine(LogLevel.Error, lazyLine, depth);
+
+        public void CriticalLog(Func<string> lazyLine, int depth = 0)
+            => LogLine(LogLevel.Critical, lazyLine, depth);
+
+        public void LogLine(LogLevel level, Func<string> lazyLine, int depth = 0)
+        {
+            var isEnabled = _aiLogger?.IsEnabled(level) == false ||
+                            _systemLogger?.IsEnabled(level) == false;
+
+            // Remove lazy lines that are deeper than the current depth
+            while (_lazyLines.Count > 0 && _lazyLines.Last.Value.Depth > depth)
+                _lazyLines.RemoveLast();
+
+            if (isEnabled)
+            {
+                // Print lazy lines that are less deep than the current depth
+                while (_lazyLines.Count > 0 && _lazyLines.First.Value.Depth < depth)
+                    PrintAndRemove(_lazyLines.First, level);
+
+                // Remove lazy lines at the same depth as the current one
+                while (_lazyLines.Count > 0 && _lazyLines.First.Value.Depth == depth)
+                    _lazyLines.RemoveFirst();
+
+                if (_lazyLines.Count == 0)
+                    lastDepth = -1;
+
+                Print(lazyLine, depth, level);
+            }
+            else
+            {
+                _lazyLines.AddLast(new LazyLine(lazyLine, depth));
+            }
+
+            lastDepth = _lazyLines.Count == 0 ? -1 : depth;
+        }
+
+        public void Dispose()
+        {
+            _lazyLines.Clear();
+        }
+
+        void PrintAndRemove(LinkedListNode<LazyLine> node, LogLevel level)
+        {
+            Print(
+                lazyLine: node.Value.Line,
+                depth: node.Value.Depth,
+                level: level);
+
+            _lazyLines.Remove(node);
+        }
+
+        void Print(Func<string> lazyLine, int depth, LogLevel level)
+        {
+            var padding = StringUtils.GetPadding(depth);
+            var line = $"{padding}{lazyLine()}";
+
+            _aiLogger?.Log(level, line);
+            _systemLogger?.Log(level, line);
+        }
+
+        private class LazyLine
+        {
+            public Func<string> Line { get; }
+            public int Depth { get; }
+
+            public LazyLine(Func<string> line, int depth)
+            {
+                Line = line;
+                Depth = depth;
+            }
+        }
+    }
+}
