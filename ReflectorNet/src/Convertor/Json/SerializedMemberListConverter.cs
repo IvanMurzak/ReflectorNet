158e9ddc825ac044653608a474b69c256a819bd5
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-10-22 23:36:00 -0700

    feat: add SerializedMember type to defined types in converters

--- ReflectorNet/src/Convertor/Json/SerializedMemberListConverter.cs
+++ ReflectorNet/src/Convertor/Json/SerializedMemberListConverter.cs
@@ -38,6 +38,7 @@ namespace com.IvanMurzak.ReflectorNet.Json
         };
         public override IEnumerable<Type> GetDefinedTypes()
         {
+            yield return typeof(SerializedMemberList);
             yield return typeof(SerializedMember);
         }
 
