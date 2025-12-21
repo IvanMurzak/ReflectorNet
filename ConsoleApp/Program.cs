f9d3c3ba7dcbcb8fe0f557480e9164ad3d9ccd3a
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-12-21 00:58:52 -0800

    Add schema reflection and sample models to Program.cs
    Introduced usage of Reflector to generate and print argument and return schemas for a sample method. Added sample classes, structs, and enums (Data, Result, EnumFlag, GeoPoint, PlayerProfile, GameState<T>) to demonstrate schema generation, including recursive and generic types.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,6 +9,80 @@ using com.IvanMurzak.ReflectorNet;
 using com.IvanMurzak.ReflectorNet.Model;
 using com.IvanMurzak.ReflectorNet.Tests;
 using com.IvanMurzak.ReflectorNet.Tests.Model;
+using System.ComponentModel;
 using System.Reflection;
 
 Console.WriteLine($"Do nothing");
+
+var reflector = new Reflector();
+var methodInfo = typeof(Sample).GetMethod(nameof(Sample.Command));
+
+var argumentsSchema = reflector.GetArgumentsSchema(methodInfo);
+var outputSchema = reflector.GetReturnSchema(methodInfo);
+
+Console.WriteLine("Arguments Schema:");
+Console.WriteLine(argumentsSchema.ToJsonString(new System.Text.Json.JsonSerializerOptions { WriteIndented = true }));
+Console.WriteLine("Output Schema:");
+Console.WriteLine(outputSchema.ToJsonString(new System.Text.Json.JsonSerializerOptions { WriteIndented = true }));
+
+var schemaJson = reflector.GetSchema<GameState<PlayerProfile>>();
+
+Console.WriteLine("Generic Schema:");
+Console.WriteLine(schemaJson.ToJsonString(new System.Text.Json.JsonSerializerOptions { WriteIndented = true }));
+
+public static class Sample
+{
+    // [McpServerTool]
+    public static Result Command(List<Data> dataList, EnumFlag flag, Data? optionalData = null)
+    {
+        return new Result();
+    }
+}
+
+
+public class Data
+{
+    public int? optionalInt;
+    [Description("THIS IS THE DEMO DESCRIPTION.")]
+    public string address;
+    public List<Data> list; // recursive
+}
+
+public struct Result
+{
+    public bool isDone;
+    public string? errorMessage;
+}
+
+[Description("DEMO ENUM DESCRIPTION.")]
+public enum EnumFlag { red, green, yellow }
+
+
+
+
+
+
+
+// A simple value type
+public struct GeoPoint
+{
+    public double Latitude;
+    public double Longitude;
+}
+
+// A complex entity
+public class PlayerProfile
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string[] Badges { get; set; } // Array
+    public Dictionary<string, int> Stats { get; set; } // Dictionary
+}
+
+// A generic wrapper (common in Game State or API responses)
+public class GameState<T>
+{
+    public long Timestamp { get; set; }
+    public T Player { get; set; }
+    public List<GeoPoint> Checkpoints { get; set; } // List of Structs
+}
