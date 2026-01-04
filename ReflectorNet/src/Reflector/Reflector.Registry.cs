97196e522542e2db58db462d69be0e8e1e7b6bad
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2026-01-04 05:40:13 -0800

    fix: Refactor blacklisted type checks to use IsTypeBlacklisted method for improved clarity

--- ReflectorNet/src/Reflector/Reflector.Registry.cs
+++ ReflectorNet/src/Reflector/Reflector.Registry.cs
@@ -132,7 +132,7 @@ namespace com.IvanMurzak.ReflectorNet
                 }
 
                 // Check if any implemented interface is blacklisted
-                if (type.GetInterfaces().Any(x => _blacklistedTypes.ContainsKey(x)))
+                if (type.GetInterfaces().Any(x => IsTypeBlacklisted(x)))
                     return true;
 
                 // Check if it's an array and the element type is blacklisted
@@ -146,7 +146,7 @@ namespace com.IvanMurzak.ReflectorNet
                 // Check if it's a generic type and any type argument is blacklisted
                 if (type.IsGenericType)
                 {
-                    if (type.GetGenericArguments().Any(x => IsTypeBlacklisted(x)))
+                    if (type.GetGenericArguments().Any(IsTypeBlacklisted))
                         return true;
                 }
 
