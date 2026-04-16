61ad5214199aede1b2a4672265cb9ac7c94cfb73
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2026-04-16 00:01:44 -0700

    fix: downgrade System.Text.Json version to 8.0.5 for compatibility

--- ReflectorNet/src/Utils/Json/JsonSchema.cs
+++ ReflectorNet/src/Utils/Json/JsonSchema.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text.Json.Nodes;
+
 using System.Threading.Tasks;
 using com.IvanMurzak.ReflectorNet.Json;
 
